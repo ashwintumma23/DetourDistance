62e94ea9624e606fc4b2bc4b602d196cd3501d9f
Ashwin Tumma <ashwin.tumma23@gmail.com>
2015-09-28 13:13:29 -0400

    Accepting User input

--- calculateDetourDistance.py
+++ calculateDetourDistance.py
@@ -1,1 +1,9 @@
-print "Calculate Distance"
+##########################################################################3
+# Program to find the shorter detour distance 
+##########################################################################3
+
+def main():
+	latitude1 = raw_input("Enter Latitude of Point A")
+	print latitude1
+
+main()
