4a4d271354df6bd7c2b087f6885d454301f79fb9
Ashwin Tumma <ashwin.tumma23@gmail.com>
2015-09-28 22:56:05 -0400

    Manhattan Example Checkin

--- README.md
+++ README.md
@@ -2,3 +2,32 @@
 [![Code Climate](https://codeclimate.com/repos/56096f1669568012ce0004dc/badges/1110086f7d7debdb2c12/gpa.svg)](https://codeclimate.com/repos/56096f1669568012ce0004dc/feed)
 
 Calculate Detour distance
+
+```
+Point A: Empire State Building, 34th Street
+40.74877690495834, -73.98543119430542
+
+Point B: Central Park, 111th Street
+40.798314488417, -73.95227909088135
+
+Point C: Times Square, 42nd Street
+40.75896043391528, -73.98468017578125
+
+Point D: Rockeffeler Center, 49th Street
+40.758245280893014, -73.97802829743432
+
+ashwin@ashwin-Studio-1537:~/DetourDistance$ python calculateDetourDistance.py 
+
+Enter Latitude of Point A: 40.74877690495834 
+Enter Longitude of Point A: -73.98543119430542
+Enter Latitude of Point B: 40.798314488417 
+Enter Longitude of Point B: -73.95227909088135
+Enter Latitude of Point C: 40.75896043391528 
+Enter Longitude of Point C: -73.98468017578125
+Enter Latitude of Point D: 40.758245280893014 
+Enter Longitude of Point D: -73.97802829743432
+
+Detour Distance: 0.298216148068 miles
+Path: A -> C -> D -> B
+
+```
--- calculateDetourDistance.py
+++ calculateDetourDistance.py
@@ -1,9 +1,69 @@
 ##########################################################################3
 # Program to find the shorter detour distance 
 ##########################################################################3
+import math
+
+
+def distanceBetweenPoints(latitude1, longitude1, latitude2, longitude2):
+	# The input values are in degrees, but Haversine needs the values to be 
+	# computed in radians
+	
+	latitude1 = degreesToRadians(latitude1)
+	longitude1 = degreesToRadians(longitude1)
+	latitude2 = degreesToRadians(latitude2)
+	longitude2 = degreesToRadians(longitude2)
+
+	latDifference = latitude2 - latitude1
+	longDifference = longitude2 - longitude1
+		
+	alpha  = math.pow(math.sin(latDifference/2),2) + math.cos(latitude1) * math.cos(latitude2) * math.pow(math.sin(longDifference/2),2);
+	c  = 2 * math.atan2(math.sqrt(alpha),math.sqrt(1-alpha)); 
+	
+	radiusOfEarth = 3961
+	return c * radiusOfEarth
+
+def degreesToRadians(degrees):
+	return degrees * math.pi / 180;
 
 def main():
-	latitude1 = raw_input("Enter Latitude of Point A")
-	print latitude1
 
+	# Latitude, Longitude for Point A
+	latitudeA = float(raw_input("Enter Latitude of Point A: "))
+	longitudeA = float(raw_input("Enter Longitude of Point A: "))
+
+	# Latitude, Longitude for Point B
+	latitudeB = float(raw_input("Enter Latitude of Point B: "))
+	longitudeB = float(raw_input("Enter Longitude of Point B: "))
+	
+	# Latitude, Longitude for Point C
+	latitudeC = float(raw_input("Enter Latitude of Point C: "))
+	longitudeC = float(raw_input("Enter Longitude of Point C: "))
+
+	# Latitude, Longitude for Point D
+	latitudeD = float(raw_input("Enter Latitude of Point D: "))
+	longitudeD = float(raw_input("Enter Longitude of Point D: "))
+
+	# Calculate the distance between points
+	distAB = distanceBetweenPoints(latitudeA, longitudeA, latitudeB, longitudeB)
+	distAC = distanceBetweenPoints(latitudeA, longitudeA, latitudeC, longitudeC)
+	distCD = distanceBetweenPoints(latitudeC, longitudeC, latitudeD, longitudeD)
+	distBD = distanceBetweenPoints(latitudeB, longitudeB, latitudeD, longitudeD)
+
+	# Compute the two possible detour paths, ACDB and CABD
+	# First detour ACDB - AB
+	first = (distAC + distCD + distBD) - distAB
+	
+	# Second detour CABD - CD
+	second = (distAC + distAB + distBD) - distCD
+	
+	if first < second: 
+		print "Detour Distance: " + str(first) + " miles"
+		print "Path: A -> C -> D -> B" 	
+	elif first == second:
+		print "Detour Distance: " + str(first)  + " miles"
+		print "Either of the Paths: A -> C -> D -> B or C -> A -> B -> D" 	
+	else: 
+		print "Detour Distance: " + str(second) + " miles"
+		print "Path: C -> A -> B -> D" 	
+		
 main()
